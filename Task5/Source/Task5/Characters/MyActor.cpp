8767aa510bc0593fb4f11f63a7ebcd606d2158ac
Hyun <hnjogo@gmail.com>
2025-08-27 19:30:46 +0900

    next 좌표에 - 가능성을 추가함

--- Task5/Source/Task5/Characters/MyActor.cpp
+++ Task5/Source/Task5/Characters/MyActor.cpp
@@ -10,25 +10,40 @@ AMyActor::AMyActor()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	Coords.Reserve(11);
-	Coords.Add(FVector(0, 0,0));
+	Coords.Add(FVector(0, 0, 0));
 }
 
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 	for (int i = 1; i < 11; i++)
 	{
 		FVector next = Coords[i - 1];
-		next.X += Step();
-		next.Y += Step();
+		if (Task5::Utils::IsSuccess50Percent())
+		{
+			next.X += Step();
+		}
+		else
+		{
+			next.X -= Step();
+		}
+
+		if (Task5::Utils::IsSuccess50Percent())
+		{
+			next.Y += Step();
+		}
+		else
+		{
+			next.Y -= Step();
+		}
 		Coords.Add(next);
 	}
 
 	// ToString : FString 반환
 	// UE_LOG에서 Const TCHAR* 를 요구하기에 
 	// Operator* 연산자를 이용하여 해당 타입으로 변환
-	UE_LOG(LogCoord, Log, TEXT("현재 위치 : %s"),*Coords[Idx].ToString());
+	UE_LOG(LogCoord, Log, TEXT("현재 위치 : %s"), *Coords[Idx].ToString());
 }
 
 int32_t AMyActor::Step()
@@ -65,7 +80,7 @@ void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if(IsArrived == false)
+	if (IsArrived == false)
 		Move();
 }
 
